bb1e9e946acc5298e711bcac8e1f39153d249754
Romina D-R <rdr2711@yahoo.com>
2018-06-26 15:13:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 $(document).ready(function (){
 });
 
-let seachTerms = ''
+const searchTerms = ''
 let url = ''
 
 function searchRepositories(searchTerms) {
   $('#searchTerms').val()
-  $.get ('https://api.github.com/search/repositories?q=') ({
-    url: 'https://developer.github.com/v3/',
-    dataType: "json",
-    data: { 
-      respository_name: 'Name',
-      q: searchTerms, 
-    },
-    success: resp => {
-        url = resp.data['0'].embed_url
-        $('#results').html(`<iframe src=${url}>`)
+  $.get ('https://api.github.com/search/repositories?q=')
+    $('#searchTerms'); , data => {
+      $('#results').html();
     }
-  })
+    
 }
-
-$('#submitButton').click(function() {
-    input = $('#searchText').val();
-    console.log(searchTerms)
-    searchRepositories(searchTerms)
-})
