06fa083729d5a224b5f6a0bbf62f379572d39bcc
Romina D-R <rdr2711@yahoo.com>
2018-06-26 14:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function findGIFS(input) {
     $.ajax({
     url: 'https://api.giphy.com/v1/gifs/search',
     dataType: "json", // informs $.ajax what the type of data we are expecting back will be
-    data: { // holds our request parameters in key: value form
+    data: { 
       api_key: 'CqNVJSuyFVcqviUl6VHCtqea80w0ZCXX', // our app specific key
       q: input, // the search query itself
       limit: 3, // limit to 3 GIF result
@@ -16,13 +16,7 @@ function findGIFS(input) {
     },
     success: resp => {
         url = resp.data['0'].embed_url
-        $('#firstDiv').html(`<iframe src=${url}>`)
-
-        url = resp.data['1'].embed_url
-        $('#secondDiv').html(`<iframe src=${url}>`)
-
-        url = resp.data['2'].embed_url
-        $('#thirdDiv').html(`<iframe src=${url}>`)
+        $('#results').html(`<iframe src=${url}>`)
     }
   })
 }
