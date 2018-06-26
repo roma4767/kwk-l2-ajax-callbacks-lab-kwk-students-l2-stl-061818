2e55bbdaf8a6ed3d41cf02f636c902c16ee74bbc
Romina D-R <rdr2711@yahoo.com>
2018-06-26 14:49:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function findGIFS(input) {
     url: 'https://api.giphy.com/v1/gifs/search',
     dataType: "json", // informs $.ajax what the type of data we are expecting back will be
     data: { 
-      api_key: 'CqNVJSuyFVcqviUl6VHCtqea80w0ZCXX', // our app specific key
-      q: input, // the search query itself
+      api_key: 'CqNVJSuyFVcqviUl6VHCtqea80w0ZCXX', /
+      q: input, // the se/ our app specific keyarch query itself
       limit: 3, // limit to 3 GIF result
       rating: 'PG-13' // limits our results to everything PG-13 and under 
     },
