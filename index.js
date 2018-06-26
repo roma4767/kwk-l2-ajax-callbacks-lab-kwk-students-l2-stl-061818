c78a169ce61abeba4865f30d1c19e41aa5dc9960
Romina D-R <rdr2711@yahoo.com>
2018-06-26 15:17:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let url = ''
 function searchRepositories(searchTerms) {
   const searchTerms = $('#searchTerms').val()
   $.get ('https://api.github.com/search/repositories?q=')
-    $('#searchTerms'); , data => {
+  $('#searchTerms') ; , data => {
       $('#results').html();
     }
     
