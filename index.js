73e829fd767dc89fc834ca6739a9ece52e5349ba
Romina D-R <rdr2711@yahoo.com>
2018-06-26 15:27:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ let url = ''
 
 function searchRepositories(searchTerms) {
   const searchTerms = $('#searchTerms').val()
-  $.get ('https://api.github.com/search/repositories?q=')
-  $('#searchTerms') ; , data => {
+  $.get ('https://api.github.com/search/repositories?q=${#searchTerms} ; , data => {
       $('#results').html();
-    }
+    })
     
 }
 
-let renderSearchResults = (data) => dta.items.map(results => )
+let renderSearchResults = (data) => dta.items.map(results => renderSearchResults(result))
+
+let renderSearchResults = (result) =>
