6b27eb062598a3d6b02a7ab7f230b65ed5094454
Romina D-R <rdr2711@yahoo.com>
2018-06-26 15:14:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 $(document).ready(function (){
 });
 
-const searchTerms = ''
+
 let url = ''
 
 function searchRepositories(searchTerms) {
-  $('#searchTerms').val()
+  const searchTerms = ''$('#searchTerms').val()
   $.get ('https://api.github.com/search/repositories?q=')
     $('#searchTerms'); , data => {
       $('#results').html();
