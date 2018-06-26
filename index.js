67a157541a49539893c5d400f248b8caebf7d41c
Romina D-R <rdr2711@yahoo.com>
2018-06-26 15:30:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 $(document).ready(function (){
 });
+
+
+let url = ''
+
+function searchRepositories(searchTerms) {
+  const searchTerms = $('#searchTerms').val()
+  $.get (`https://api.github.com/search/repositories?q=${#searchTerms}` , data => {
+      $('#results').html();
+    })
+    
+}
+
+let renderSearchResults = (data) => dta.items.map(results => renderSearchResults(result))
+
+let renderSearchResults = (result) => 
